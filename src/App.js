52aad1d4b242e537d7dd3de1425eb560d5e95847
Matheus Santos <matheus.santos@ilegra.com>
2019-11-21 14:36:03 -0300

    Added navigation routes.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Routes from './routes';
 import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
 import './styles.css';
@@ -6,7 +7,7 @@ import './styles.css';
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
